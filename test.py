784006a37b377bf77bbb127d49cbe680f7d031d1
gogodjames <jamesgogod@gmail.com>
2025-08-04 15:52:24 -0700

    added planet name, discovery method, orbital period, and distance as important parameters the search f unction should return

--- test.py
+++ test.py
@@ -5,10 +5,11 @@ tap_service = vo.dal.TAPService("https://exoplanetarchive.ipac.caltech.edu/TAP")
 
 ex_query = """
     SELECT TOP 5
-    *
+    pl_name, discoverymethod, pl_orbper,sy_dist
     FROM pscomppars 
     """
 result = tap_service.search(ex_query)
 
 print(result.to_table().colnames)
 print(np.array(result).shape)
+print(result)
