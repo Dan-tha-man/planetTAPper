67dd01bf2968cfb88cf5f327b801c8ec0fe6736a
hankcht <hanktang930809@gmail.com>
2025-08-05 11:13:51 -0700

    add search_planet_by_name.py

---
+++ search_planet_by_name.py
@@ -0,0 +1,29 @@
+import pyvo as vo
+import numpy as np
+import pandas
+from planetTAPper import Planet
+
+tap_service = vo.dal.TAPService("https://exoplanetarchive.ipac.caltech.edu/TAP")
+
+def search_planet_by_name(name):
+    
+    ex_query = f"""
+        SELECT TOP 1
+        pl_name,pl_orbper,pl_radj,pl_massj,pl_orbeccen, hostname,st_spectype,st_teff,st_rad,st_mass,st_rotp,sy_dist
+        FROM pscomppars
+        WHERE pl_name = '{name}'
+        """
+    result = tap_service.search(ex_query)
+    planet = Planet()
+    columns = result.to_table().colnames
+    print(result)
+    for column in columns:
+        match column:
+            case 'pl_name':
+                planet.name = result.to_table()['pl_name'][0] 
+            case 'pl_orbper':
+                planet.period = result.to_table()['pl_orbper'][0]
+                
+if __name__ == "__main__":
+    planet_name = "Kepler-334 b"
+    search_planet_by_name(planet_name)
