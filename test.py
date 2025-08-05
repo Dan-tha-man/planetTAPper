21d97c8fb822287b0846142bdb470f395f271275
hankcht <hanktang930809@gmail.com>
2025-08-05 10:06:27 -0700

    change query

--- test.py
+++ test.py
@@ -4,11 +4,12 @@ import numpy as np
 tap_service = vo.dal.TAPService("https://exoplanetarchive.ipac.caltech.edu/TAP")
 
 ex_query = """
-    SELECT TOP 5
+    SELECT top 1
     *
     FROM pscomppars 
     """
 result = tap_service.search(ex_query)
 
 print(result.to_table().colnames)
+print(result.to_table())
 print(np.array(result).shape)
