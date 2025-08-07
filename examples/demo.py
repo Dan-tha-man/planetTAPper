04cba284ce078f115a46c2d3a8f8eec5480fa3cc
gogodjames <jamesgogod@gmail.com>
2025-08-07 15:17:33 -0700

    added my fav planet

--- examples/demo.py
+++ examples/demo.py
@@ -12,6 +12,7 @@ enable_plot = False
 # --- Input for Search By Name ---
 object_name = 'Kepler-334 b' # could be star or planet
 # SWEEPS-11 b (Hank's choice)
+# Barnard b (Gogod's choice)
 
 
 # --- Input for Search By Parameters ---
